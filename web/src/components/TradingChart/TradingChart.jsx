f60f8cd8703c817e95e90f43f66ee4bebb071b85
Tim Reznich <tim.reznich@gmail.com>
2018-01-18 13:20:17 +0200

    feat(Utils): connected TradingView graphs

--- web/src/components/TradingChart/TradingChart.jsx
+++ web/src/components/TradingChart/TradingChart.jsx
@@ -1,28 +1,45 @@
 // @flow
-import React from 'react';
-import type { Node } from 'react';
+import React, { Component } from 'react';
 
-import { TradingChartContainer } from './styled';
-
-// type Props = {
-//   /** List of all orders */
-//   orders: Array<Order>,
-//   /**
-//    * Function that is called whenever order clicked
-//    * */
-//   onClick: Function,
-//   /**
-//    * Function that is called whenever order canceled
-//    * */
-//   onCancel: Function,
-// };
+type Props = {};
 
 /**
  * Trading Chart
  * @version 1.0.0
  * @author [Tim Reznich](https://github.com/imbaniac)
  */
+export default class extends Component<Props> {
+  componentDidMount() {
+    // eslint-disable-next-line
+    const widget = new window.TradingView.widget({
+      height: '500px',
+      width: '100%',
+      debug: true,
+      symbol: 'AAPL',
+      interval: 'D',
+      container_id: 'tv_chart_container',
+      datafeed: new window.Datafeeds.UDFCompatibleDatafeed('https://demo_feed.tradingview.com'),
+      library_path: 'charting_library/',
+      locale: getParameterByName('lang') || 'en',
+      drawings_access: { type: 'black', tools: [{ name: 'Regression Trend' }] },
+      disabled_features: ['use_localstorage_for_settings'],
+      enabled_features: ['study_templates'],
+      charts_storage_url: 'http://saveload.tradingview.com',
+      charts_storage_api_version: '1.1',
+      client_id: 'tradingview.com',
+      user_id: 'public_user_id',
+    });
+    window.tv_widget = widget;
+  }
 
-const UserOrders = (): Node => <TradingChartContainer />;
+  render() {
+    return <div id="tv_chart_container" />;
+  }
+}
 
-export default UserOrders;
+function getParameterByName(name) {
+  const newName = name.replace(/[[]/, '\\[').replace(/[\]]/, '\\]');
+  const regex = new RegExp(`[\\?&]${newName}=([^&#]*)`);
+  const results = regex.exec(window.location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
