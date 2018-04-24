2da15d36ec3600bef6cd72815bda550d6d33e0cb
Tim Reznich <tim@rezni.ch>
2018-04-24 13:37:15 +0300

    feat: increased max amount to 0.5 eth

--- core/src/utils/web3.js
+++ core/src/utils/web3.js
@@ -88,6 +88,7 @@ export const connectionStatuses = {
 export const NODE_ADDRESS = '0x004e344251110fa1cb09aa31c95c6598ed07dce6';
 // export const NODE_ADDRESS = '0x5409ed021d9299bf6814279a6a1411a7e866a631';
 export const SMALLEST_AMOUNT = 0.005;
+export const BIGGEST_AMOUNT = 0.5;
 // Hardcoded - around 1$ for now ($400/eth)
 export const TRANSACTION_FEE = 0.0025;
 // Percentage fee (our fee)
