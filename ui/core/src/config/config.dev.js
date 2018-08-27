d23b7ced4464e569d01c8e16679d71d4d319497c
VladimirPal <mail.vpal@gmail.com>
2018-08-27 16:47:33 +0300

    refactor: remove config module from core
    ISSUES CLOSED: #14

--- ui/core/src/config/config.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiUrl: 'http://localhost:3001/api',
-  // apiUrl: 'https://api.instex.io/api',
-  socketUrl: 'http://localhost:5001',
-  // socketUrl: 'https://socket.instex.io:8081',
-  useMock: false,
-  useSentry: false,
-};
--- ui/core/src/config/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./config.prod');
-} else if (process.env.NODE_ENV === 'development') {
-  module.exports = require('./config.dev');
-}
