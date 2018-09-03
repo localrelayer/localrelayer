ec5f9d4f05b70fc1c56af7afad312e5eb856a24d
WaVe41 <farellmorrison@gmail.com>
2018-09-03 20:05:28 +0300

    feat(ui): add storybook for TradingPageLayout component
    ISSUES CLOSED: #25

--- ui/web/src/components/TradingPageLayout/TradingPageLayout.jsx
+++ ui/web/src/components/TradingPageLayout/TradingPageLayout.jsx
@@ -7,177 +7,177 @@ import {
 
 import {
   layout as assetPairCardLayout,
-} from 'web-components/AssetPairCard';
+} from '../AssetPairCard';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 const layoutXs = [
   assetPairCardLayout.xs,
   {
-    i: 'buySell', x: 1, y: 3, w: 1, h: 9, static: true,
+    i: 'buySell', x: 1, y: 3, w: 1, h: 9, static: false,
   },
   {
-    i: 'orderBook', x: 0, y: 12, w: 1, h: 16, static: true,
+    i: 'orderBook', x: 0, y: 12, w: 1, h: 16, static: false,
   },
   {
-    i: 'balance', x: 1, y: 28, w: 1, h: 6, static: true,
+    i: 'balance', x: 1, y: 28, w: 1, h: 6, static: false,
   },
   {
-    i: 'history', x: 0, y: 45, w: 1, h: 11, static: true,
+    i: 'history', x: 0, y: 45, w: 1, h: 11, static: false,
   },
   {
-    i: 'userOrders', x: 0, y: 34, w: 1, h: 11, static: true,
+    i: 'userOrders', x: 0, y: 34, w: 1, h: 11, static: false,
   },
   {
-    i: 'chart', x: 0, y: 0, w: 0, h: 0, static: true,
+    i: 'chart', x: 0, y: 0, w: 0, h: 0, static: false,
   },
   {
-    i: 'news', x: 0, y: 56, w: 1, h: 7, static: true,
+    i: 'news', x: 0, y: 56, w: 1, h: 7, static: false,
   },
 ];
 
 
 const layoutSm = [
   {
-    i: 'orderBook', x: 1, y: 0, w: 1, h: 18, static: true,
+    i: 'orderBook', x: 1, y: 0, w: 1, h: 18, static: false,
   },
   {
-    i: 'balance', x: 0, y: 3, w: 1, h: 6, static: true,
+    i: 'balance', x: 0, y: 3, w: 1, h: 6, static: false,
   },
 
   {
-    i: 'chart', x: 0, y: 18, w: 2, h: 12, static: true,
+    i: 'chart', x: 0, y: 18, w: 2, h: 12, static: false,
   },
   {
-    i: 'userOrders', x: 0, y: 30, w: 2, h: 11, static: true,
+    i: 'userOrders', x: 0, y: 30, w: 2, h: 11, static: false,
   },
   {
-    i: 'assetPairCard', x: 0, y: 0, w: 1, h: 3, static: true,
+    i: 'assetPairCard', x: 0, y: 0, w: 1, h: 3, static: false,
   },
   {
-    i: 'buySell', x: 0, y: 3, w: 1, h: 9, static: true,
+    i: 'buySell', x: 0, y: 3, w: 1, h: 9, static: false,
   },
   {
-    i: 'history', x: 0, y: 41, w: 2, h: 11, static: true,
+    i: 'history', x: 0, y: 41, w: 2, h: 11, static: false,
   },
   {
-    i: 'news', x: 0, y: 52, w: 1, h: 7, static: true,
+    i: 'news', x: 0, y: 52, w: 1, h: 7, static: false,
   },
 ];
 
 const layoutMd = [
   {
-    i: 'orderBook', x: 7, y: 0, w: 3, h: 19, static: true,
+    i: 'orderBook', x: 7, y: 0, w: 3, h: 19, static: false,
   },
   {
-    i: 'balance', x: 0, y: 13, w: 3.5, h: 6, static: true,
+    i: 'balance', x: 0, y: 13, w: 3.5, h: 6, static: false,
   },
 
   {
-    i: 'chart', x: 0, y: 0, w: 7, h: 10, static: true,
+    i: 'chart', x: 0, y: 0, w: 7, h: 10, static: false,
   },
   {
-    i: 'userOrders', x: 3.5, y: 19, w: 6.5, h: 11, static: true,
+    i: 'userOrders', x: 3.5, y: 19, w: 6.5, h: 11, static: false,
   },
   {
-    i: 'assetPairCard', x: 0, y: 10, w: 3.5, h: 3, static: true,
+    i: 'assetPairCard', x: 0, y: 10, w: 3.5, h: 3, static: false,
   },
   {
-    i: 'buySell', x: 3.5, y: 10, w: 3.5, h: 9, static: true,
+    i: 'buySell', x: 3.5, y: 10, w: 3.5, h: 9, static: false,
   },
   {
-    i: 'history', x: 0, y: 19, w: 3.5, h: 11, static: true,
+    i: 'history', x: 0, y: 19, w: 3.5, h: 11, static: false,
   },
   {
-    i: 'news', x: 0, y: 30, w: 3.5, h: 7, static: true,
+    i: 'news', x: 0, y: 30, w: 3.5, h: 7, static: false,
   },
 
 ];
 
 const layoutLg = [
   {
-    i: 'orderBook', x: 9, y: 0, w: 3, h: 14, static: true,
+    i: 'orderBook', x: 9, y: 0, w: 3, h: 14, static: false,
   },
   {
-    i: 'balance', x: 0, y: 12, w: 3, h: 6, static: true,
+    i: 'balance', x: 0, y: 12, w: 3, h: 6, static: false,
   },
   {
-    i: 'news', x: 0, y: 18, w: 3, h: 7, static: true,
-  },
-
-  {
-    i: 'chart', x: 3, y: 0, w: 6, h: 14, static: true,
-  },
-  {
-    i: 'userOrders', x: 3, y: 14, w: 6, h: 11, static: true,
+    i: 'news', x: 0, y: 18, w: 3, h: 7, static: false,
   },
 
   {
-    i: 'assetPairCard', x: 0, y: 0, w: 3, h: 3, static: true,
+    i: 'chart', x: 3, y: 0, w: 6, h: 14, static: false,
   },
   {
-    i: 'buySell', x: 0, y: 3, w: 3, h: 9, static: true,
+    i: 'userOrders', x: 3, y: 14, w: 6, h: 11, static: false,
+  },
+
+  {
+    i: 'assetPairCard', x: 0, y: 0, w: 3, h: 3, static: false,
   },
   {
-    i: 'history', x: 9, y: 14, w: 3, h: 11, static: true,
+    i: 'buySell', x: 0, y: 3, w: 3, h: 9, static: false,
+  },
+  {
+    i: 'history', x: 9, y: 14, w: 3, h: 11, static: false,
   },
 ];
 
 const layoutLg2 = [
   {
-    i: 'orderBook', x: 19, y: 0, w: 5, h: 14, static: true,
+    i: 'orderBook', x: 19, y: 0, w: 5, h: 14, static: false,
   },
   {
-    i: 'balance', x: 0, y: 12, w: 5, h: 7, static: true,
-  },
-
-  {
-    i: 'chart', x: 5, y: 0, w: 14, h: 14, static: true,
-  },
-  {
-    i: 'userOrders', x: 5, y: 14, w: 14, h: 11, static: true,
+    i: 'balance', x: 0, y: 12, w: 5, h: 7, static: false,
   },
 
   {
-    i: 'assetPairCard', x: 0, y: 0, w: 5, h: 3, static: true,
+    i: 'chart', x: 5, y: 0, w: 14, h: 14, static: false,
   },
   {
-    i: 'buySell', x: 0, y: 3, w: 5, h: 9, static: true,
+    i: 'userOrders', x: 5, y: 14, w: 14, h: 11, static: false,
+  },
+
+  {
+    i: 'assetPairCard', x: 0, y: 0, w: 5, h: 3, static: false,
   },
   {
-    i: 'history', x: 19, y: 14, w: 5, h: 11, static: true,
+    i: 'buySell', x: 0, y: 3, w: 5, h: 9, static: false,
   },
   {
-    i: 'news', x: 0, y: 19, w: 5, h: 7, static: true,
+    i: 'history', x: 19, y: 14, w: 5, h: 11, static: false,
+  },
+  {
+    i: 'news', x: 0, y: 19, w: 5, h: 7, static: false,
   },
 ];
 
 const layoutLg3 = [
   {
-    i: 'orderBook', x: 10, y: 0, w: 2, h: 20, static: true,
+    i: 'orderBook', x: 10, y: 0, w: 2, h: 20, static: false,
   },
   {
-    i: 'balance', x: 0, y: 12, w: 2, h: 7, static: true,
-  },
-
-  {
-    i: 'chart', x: 2, y: 0, w: 8, h: 20, static: true,
-  },
-  {
-    i: 'userOrders', x: 2, y: 20, w: 8, h: 11, static: true,
+    i: 'balance', x: 0, y: 12, w: 2, h: 7, static: false,
   },
 
   {
-    i: 'assetPairCard', x: 0, y: 0, w: 2, h: 3, static: true,
+    i: 'chart', x: 2, y: 0, w: 8, h: 20, static: false,
   },
   {
-    i: 'buySell', x: 0, y: 3, w: 2, h: 9, static: true,
+    i: 'userOrders', x: 2, y: 20, w: 8, h: 11, static: false,
+  },
+
+  {
+    i: 'assetPairCard', x: 0, y: 0, w: 2, h: 3, static: false,
   },
   {
-    i: 'history', x: 10, y: 20, w: 2, h: 11, static: true,
+    i: 'buySell', x: 0, y: 3, w: 2, h: 9, static: false,
   },
   {
-    i: 'news', x: 0, y: 19, w: 2, h: 7, static: true,
+    i: 'history', x: 10, y: 20, w: 2, h: 11, static: false,
+  },
+  {
+    i: 'news', x: 0, y: 19, w: 2, h: 7, static: false,
   },
 ];
 
@@ -217,39 +217,6 @@ const TrandgPageLayout = ({ children }: Props) => (
     onLayoutChange={() => {}}
   >
     {children}
-    {/*
-    <div key="assetPairCard">
-      <AssetPairCard />
-    </div>
-
-    <div key="balance">
-      <UserBalance />
-    </div>
-    <div key="userOrders">
-      <UserOrders />
-    </div>
-
-    <div key="chart">
-      <TradingChart />
-    </div>
-
-    <div key="orderBook">
-      <OrderBook />
-    </div>
-
-
-    <div key="buySell">
-      <BuySell />
-    </div>
-
-    <div key="history">
-      <TradingHistory />
-    </div>
-
-    <div key="news">
-      <News />
-    </div>
-    */}
   </ResponsiveReactGridLayout>
 );
 
