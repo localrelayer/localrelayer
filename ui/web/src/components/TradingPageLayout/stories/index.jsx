e7350b6d643064881806acb5c0d8994d0f022424
VladimirPal <mail.vpal@gmail.com>
2018-09-20 14:51:03 +0300

    improvement(ui): use <TradingPageLayout.Preview /> in layout story

--- ui/web/src/components/TradingPageLayout/stories/index.jsx
+++ ui/web/src/components/TradingPageLayout/stories/index.jsx
@@ -4,43 +4,8 @@ import {
 } from '@storybook/react';
 
 import TradingPageLayout from '..';
-import {
-  DottedBorder,
-} from '../styled';
 
 storiesOf('TradingPageLayout', module)
   .add('trading-page-layout-default', () => (
-    <TradingPageLayout>
-      <DottedBorder key="assetPairCard">
-        AssetPairs
-      </DottedBorder>
-
-      <DottedBorder key="balance">
-        Balance
-      </DottedBorder>
-
-      <DottedBorder key="userOrders">
-        OpenedOrders
-      </DottedBorder>
-
-      <DottedBorder key="chart">
-        Charts
-      </DottedBorder>
-
-      <DottedBorder key="orderBook">
-        OrderBook
-      </DottedBorder>
-
-      <DottedBorder key="buySell">
-        Buy/Sell
-      </DottedBorder>
-
-      <DottedBorder key="history">
-        TradingHistory
-      </DottedBorder>
-
-      <DottedBorder key="news">
-          News
-      </DottedBorder>
-    </TradingPageLayout>
+    <TradingPageLayout.Preview />
   ));
