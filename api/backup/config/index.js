58be90ca430c1d0ddcfc3ddcdc35c5c3a7b51e3d
VladimirPal <mail.vpal@gmail.com>
2018-11-12 12:53:14 +0200

    refactor(api): remove old api

--- api/backup/config/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./config.prod');
-} else if (process.env.NODE_ENV === 'test') {
-  module.exports = require('./config.test');
-} else {
-  module.exports = require('./config.dev');
-}
