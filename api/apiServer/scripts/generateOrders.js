751ca76d6106cf58fc4c21505a58f62e10f919f0
VladimirPal <mail.vpal@gmail.com>
2018-11-13 15:36:07 +0200

    feat(api): generateOrders command to generate orders

---
+++ api/apiServer/scripts/generateOrders.js
@@ -0,0 +1,59 @@
+import {
+  orderHashUtils,
+} from '0x.js';
+import {
+  generatePseudoRandomSalt,
+} from '@0x/order-utils';
+import {
+  NULL_ADDRESS,
+  getOrderConfig,
+  generateRandomMakerAssetAmount,
+  generateRandomTakerAssetAmount,
+  getRandomFutureDateInSeconds,
+  randomEthereumAddress,
+} from '../../utils';
+import {
+  Order,
+} from '../../db';
+
+
+(() => {
+  const ordersQty = 10;
+  const orderConfig = getOrderConfig();
+  const testData = {
+    makerAddress: () => randomEthereumAddress(),
+    takerAddress: () => NULL_ADDRESS,
+    makerAssetAmount: () => generateRandomMakerAssetAmount(18).toString(),
+    takerAssetAmount: () => generateRandomTakerAssetAmount(18).toString(),
+    makerAssetData: () => '0xf47261b0000000000000000000000000871dd7c2b4b25e1aa18728e9d5f2af4c4e431f5c', /* ZRX */
+    takerAssetData: () => '0xf47261b00000000000000000000000000b1ba0af832d7c05fd64161e0db78e85978e8082', /* WETH */
+    exchangeAddress: () => randomEthereumAddress(),
+    salt: () => generatePseudoRandomSalt().toString(),
+    expirationTimeSeconds: () => getRandomFutureDateInSeconds().toString(),
+    signature: () => randomEthereumAddress(),
+    completedAt: () => new Date(+(new Date()) - Math.floor(Math.random() * 10000000000)),
+  };
+  const orders = Array(ordersQty).fill().map(
+    () => {
+      const order = (
+        Object.keys(testData)
+          .reduce(
+            (acc, fieldName) => ({
+              ...acc,
+              [fieldName]: testData[fieldName](),
+            }),
+            {
+              ...orderConfig,
+              networkId: 50,
+            },
+          )
+      );
+      return {
+        ...order,
+        orderHash: orderHashUtils.getOrderHashHex(order),
+      };
+    },
+  );
+  console.log(orders);
+  Order.collection.insertMany(orders, process.exit);
+})();
--- api/utils/index.js
+++ api/utils/index.js
@@ -1,6 +1,7 @@
 import {
   RPCSubprovider,
   Web3ProviderEngine,
+  BigNumber,
 } from '0x.js';
 
 // The deployed addresses from the Ganache snapshot
@@ -46,3 +47,39 @@ export const getOrderConfig = () => ({
   makerFee: '0',
   takerFee: '0',
 });
+
+/* https://github.com/Marak/faker.js/blob/master/lib/finance.js#L223 */
+export function randomEthereumAddress() {
+  const hexadecimalSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  const randomHex = Array(40).fill().map(
+    () => (
+      hexadecimalSymbols[Math.floor(Math.random() * hexadecimalSymbols.length)]
+    ),
+  );
+  return `0x${randomHex.join('')}`;
+}
+
+export function toBaseUnit(amount, decimals) {
+  const unit = new BigNumber(10).pow(decimals);
+  return (new BigNumber(amount)).times(unit);
+}
+
+export function generateRandomMakerAssetAmount(decimals) {
+  const amount = new BigNumber(Math.round(Math.random() * 100) + 10);
+  return toBaseUnit(
+    amount,
+    decimals,
+  );
+}
+
+export function generateRandomTakerAssetAmount(decimals) {
+  const amount = new BigNumber(Math.round(Math.random() * 5) + 15);
+  return toBaseUnit(
+    amount,
+    decimals,
+  );
+}
+
+export const getRandomFutureDateInSeconds = () => (
+  new BigNumber(Date.now() + 1000000).div(900).ceil()
+);
