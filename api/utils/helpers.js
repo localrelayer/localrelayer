ac8db8117ca2da3fd69e3029c1751f607a8cd0c9
WaVe41 <farellmorrison@gmail.com>
2018-11-20 18:31:49 +0200

    test(api): add tests for orderWatcher events

--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -11,18 +11,23 @@ import {
   GANACHE_CONTRACT_ADDRESSES,
 } from './constants';
 
+const orderConfig = {
+  senderAddress: NULL_ADDRESS,
+  feeRecipientAddress: '0xc6c3d375b62d66fe0a796ed4ac30bd09ff2d1be5',
+  makerFee: '0',
+  takerFee: '0',
+};
+
+export const getOrderConfig = () => orderConfig;
+
+export const setMakerFeeForTest = (makerFee) => {
+  orderConfig.makerFee = makerFee;
+};
 
 export function toBaseUnit(amount, decimals) {
   const unit = new BigNumber(10).pow(decimals);
   return (new BigNumber(amount)).times(unit);
 }
-
-export const getOrderConfig = () => ({
-  senderAddress: NULL_ADDRESS,
-  feeRecipientAddress: '0xc6c3d375b62d66fe0a796ed4ac30bd09ff2d1be5',
-  makerFee: '0',
-  takerFee: '0',
-});
 
 export const transformBigNumberOrder = order => (
   Object.keys(order).reduce((acc, fieldName) => ({
