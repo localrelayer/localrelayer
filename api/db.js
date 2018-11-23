74c5dc8c745f8c04439800d8d78384ba8728d48b
VladimirPal <mail.vpal@gmail.com>
2018-11-23 17:13:46 +0200

    feat: store shadowed orders

--- api/db.js
+++ api/db.js
@@ -11,51 +11,47 @@ mongoose.connect(
   },
 );
 
-/* Why so much indexes? WTF? */
 const orderSchema = mongoose.Schema({
-  makerAddress: { type: String, index: true },
-  takerAddress: { type: String, index: true },
-  feeRecipientAddress: { type: String, index: true },
-  senderAddress: { type: String, index: true },
+  makerAddress: { type: String },
+  takerAddress: { type: String },
+  feeRecipientAddress: { type: String },
+  senderAddress: { type: String },
   makerAssetAmount: { type: String },
   takerAssetAmount: { type: String },
   makerFee: { type: String },
   takerFee: { type: String },
-  makerAssetData: { type: String, index: true },
-  takerAssetData: { type: String, index: true },
-  orderHash: { type: String, index: true, unique: true, dropDups: true },
+  makerAssetData: { type: String },
+  takerAssetData: { type: String },
+  orderHash: { type: String, unique: true, dropDups: true },
   expirationTimeSeconds: { type: String },
   networkId: { type: Number, index: true },
   salt: { type: String },
   exchangeAddress: { type: String },
   signature: { type: String },
-  makerAssetProxyId: { type: String, index: true },
-  takerAssetProxyId: { type: String, index: true },
-  makerAssetAddress: { type: String, index: true },
-  takerAssetAddress: { type: String, index: true },
+  makerAssetProxyId: { type: String },
+  takerAssetProxyId: { type: String },
+  makerAssetAddress: { type: String },
+  takerAssetAddress: { type: String },
   isValid: { type: Boolean },
+  isShadowed: { type: Boolean },
   error: { type: String },
-  remainingFillableMakerAssetAmount: { type: String, index: true },
-  remainingFillableTakerAssetAmount: { type: String, index: true },
-  completedAt: { type: Date, index: true },
+  remainingFillableMakerAssetAmount: { type: String },
+  remainingFillableTakerAssetAmount: { type: String },
+  completedAt: { type: Date },
 }, { versionKey: false });
-// mongoose.set('useFindAndModify', false);
 
 const assetDataSchema = mongoose.Schema({
   minAmount: {
     type: String,
-    index: true,
   },
   maxAmount: {
     type: String,
-    index: true,
   },
   precision: {
     type: Number,
   },
   assetData: {
     type: String,
-    index: true,
   },
 });
 const assetPairsSchema = mongoose.Schema({
@@ -67,7 +63,6 @@ const assetPairsSchema = mongoose.Schema({
 }, {
   versionKey: false,
 });
-// orderSchema.index({ logIndex: 1, transactionHash: 1 }, { unique: true });
 
 export const Order = mongoose.model('Order', orderSchema);
 export const AssetPair = mongoose.model('AssetPair', assetPairsSchema);
