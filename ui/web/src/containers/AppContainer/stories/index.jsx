a2eb1ac7811f5ff162869cf8c680fbd35883c1c0
VladimirPal <mail.vpal@gmail.com>
2018-11-27 20:25:17 +0200

    fix(ui): fix AppContainer storybook

--- ui/web/src/containers/AppContainer/stories/index.jsx
+++ ui/web/src/containers/AppContainer/stories/index.jsx
@@ -19,18 +19,50 @@ import {
 } from 'web-actions';
 
 import store from 'web-store';
-import Component from 'web-components/ConnectComponent';
 import AppContainer from 'web-containers/AppContainer';
 
 
 type Props = {
   route: string,
-  history: any,
+  history?: any,
 };
 
-const AppContainerStory = ({
-  route,
-  history = getHistory(
+class AppContainerStory extends React.Component<Props> {
+  componentDidMount() {
+    const {
+      route,
+      history,
+    } = this.props;
+    if (route) {
+      history.push(route);
+    }
+  }
+
+  componentWillUnmount() {
+    store.dispatch(uiActions.setUiState({
+      isAppInitializing: false,
+    }));
+  }
+
+  render() {
+    const { history } = this.props;
+    button(
+      'Toggle isAppLoading',
+      () => {
+        store.dispatch(uiActions.setUiState({
+          isAppInitializing: !store.getState().ui.isAppInitializing,
+        }));
+      },
+    );
+    return (
+      <Router history={history}>
+        <AppContainer />
+      </Router>
+    );
+  }
+}
+AppContainerStory.defaultProps = {
+  history: getHistory(
     'memory',
     {
       initialEntries: [
@@ -39,38 +71,6 @@ const AppContainerStory = ({
       initialIndex: 0,
     },
   ),
-}: Props) => {
-  /*
-   * Knob button will dissapear after change the store
-   * if register button on global scope or on DidMount,
-   * the reason is unknows, probably a bug
-  */
-  button(
-    'Toggle isAppLoading',
-    () => {
-      store.dispatch(uiActions.setUiState({
-        isAppInitializing: !store.getState().ui.isAppInitializing,
-      }));
-    },
-  );
-  if (route) {
-    history.push(route);
-  }
-  return (
-    <Component
-      componentWillUnmount={({ dispatch }) => {
-        dispatch(uiActions.setUiState({
-          isAppInitializing: false,
-        }));
-      }}
-    >
-      {() => (
-        <Router history={history}>
-          <AppContainer />
-        </Router>
-      )}
-    </Component>
-  );
 };
 
 storiesOf('Containers|AppContainer', module)
