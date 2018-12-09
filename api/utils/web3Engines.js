33973d10864e141e2a10a1d521dfb685372e7cb3
VladimirPal <mail.vpal@gmail.com>
2018-12-09 12:17:24 +0200

    improvement(api): update order-warcher version

--- api/utils/web3Engines.js
+++ api/utils/web3Engines.js
@@ -19,7 +19,8 @@ export const initWeb3ProviderEngine = (
       rpcUrl: 'https://mainnet.infura.io/v3/240b30f52dcb42e0a051a4acdfe00d8e',
     },
     42: {
-      rpcUrl: 'https://kovan.infura.io/v3/240b30f52dcb42e0a051a4acdfe00d8e',
+      rpcUrl: 'http://api.instex.io:8545',
+      // rpcUrl: 'https://kovan.infura.io/v3/240b30f52dcb42e0a051a4acdfe00d8e',
     },
     50: {
       rpcUrl: 'http://localhost:8545',
