ef932932a7557fa65f62bec9ff8070a67514dbdb
VladimirPal <mail.vpal@gmail.com>
2018-12-10 11:52:14 +0200

    improvement(ui): do not try to get a balance of null selectedAccount

--- ui/core/src/sagas/wallet.js
+++ ui/core/src/sagas/wallet.js
@@ -27,96 +27,98 @@ export function* watchWallet({
       const contractWrappers = ethApi.getWrappers(networkId);
 
       const selectedAccount = accounts.length ? accounts[0] : null;
-      const selectedAccountBalance = (
-        accounts.length
-          ? (
-            yield eff.call(web3.eth.getBalance, accounts[0])
-          ) : null
-      );
-      const balance = yield eff.all(
-        tokens.reduce(
-          (acc, tokenAddress) => ({
-            ...acc,
-            [tokenAddress]: (
-              contractWrappers.erc20Token.getBalanceAsync(
-                tokenAddress,
-                selectedAccount,
-              ).then(b => b.toString())
-            ),
-          }),
-          {},
-        ),
-      );
-
-      const allowance = yield eff.all(
-        tokens.reduce(
-          (acc, tokenAddress) => ({
-            ...acc,
-            [tokenAddress]: (
-              contractWrappers.erc20Token.getProxyAllowanceAsync(
-                tokenAddress,
-                selectedAccount,
-              ).then(b => b.toString())
-            ),
-          }),
-          {},
-        ),
-      );
-
-      const changedData = [];
-      const wallet = yield eff.select(s => s.wallet);
-      if (wallet.networkId !== networkId) {
-        changedData.push({
-          networkId,
-          networkName: utils.ethNetworks[networkId] || 'Unknown',
-        });
-      }
-      if (accounts.some((w, i) => wallet.accounts[i] !== w)) {
-        changedData.push({
-          accounts,
-        });
-      }
-      if (selectedAccount?.toLowerCase() !== wallet.selectedAccount?.toLowerCase()) {
-        changedData.push({
-          selectedAccount: selectedAccount.toLowerCase(),
-        });
-      }
-      if (selectedAccountBalance !== wallet.selectedAccountBalance) {
-        changedData.push({
-          selectedAccountBalance,
-        });
-      }
-
-      if (!R.equals(wallet.balance, balance)) {
-        changedData.push({
-          balance: {
-            _merge: true,
-            ...balance,
-          },
-        });
-      }
-
-      if (!R.equals(wallet.allowance, allowance)) {
-        changedData.push({
-          allowance: {
-            _merge: true,
-            ...allowance,
-          },
-        });
-      }
-
-      if (changedData.length) {
-        yield eff.put(
-          walletActions.setWalletState(
-            changedData.reduce(
-              (acc, data) => ({
-                ...acc,
-                ...data,
-              }),
-              {},
-            ),
+      if (selectedAccount) {
+        const selectedAccountBalance = (
+          accounts.length
+            ? (
+              yield eff.call(web3.eth.getBalance, accounts[0])
+            ) : null
+        );
+        const balance = yield eff.all(
+          tokens.reduce(
+            (acc, tokenAddress) => ({
+              ...acc,
+              [tokenAddress]: (
+                contractWrappers.erc20Token.getBalanceAsync(
+                  tokenAddress,
+                  selectedAccount,
+                ).then(b => b.toString())
+              ),
+            }),
+            {},
           ),
         );
+
+        const allowance = yield eff.all(
+          tokens.reduce(
+            (acc, tokenAddress) => ({
+              ...acc,
+              [tokenAddress]: (
+                contractWrappers.erc20Token.getProxyAllowanceAsync(
+                  tokenAddress,
+                  selectedAccount,
+                ).then(b => b.toString())
+              ),
+            }),
+            {},
+          ),
+        );
+
+        const changedData = [];
+        const wallet = yield eff.select(s => s.wallet);
+        if (wallet.networkId !== networkId) {
+          changedData.push({
+            networkId,
+            networkName: utils.ethNetworks[networkId] || 'Unknown',
+          });
+        }
+        if (accounts.some((w, i) => wallet.accounts[i] !== w)) {
+          changedData.push({
+            accounts,
+          });
+        }
+        if (selectedAccount?.toLowerCase() !== wallet.selectedAccount?.toLowerCase()) {
+          changedData.push({
+            selectedAccount: selectedAccount.toLowerCase(),
+          });
+        }
+        if (selectedAccountBalance !== wallet.selectedAccountBalance) {
+          changedData.push({
+            selectedAccountBalance,
+          });
+        }
+
+        if (!R.equals(wallet.balance, balance)) {
+          changedData.push({
+            balance: {
+              _merge: true,
+              ...balance,
+            },
+          });
+        }
+
+        if (!R.equals(wallet.allowance, allowance)) {
+          changedData.push({
+            allowance: {
+              _merge: true,
+              ...allowance,
+            },
+          });
+        }
+
+        if (changedData.length) {
+          yield eff.put(
+            walletActions.setWalletState(
+              changedData.reduce(
+                (acc, data) => ({
+                  ...acc,
+                  ...data,
+                }),
+                {},
+              ),
+            ),
+          );
+        }
       }
     } catch (err) {
       console.log(err);
