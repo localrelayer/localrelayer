0ad6083d93a48e0fd86a7fff7cfcc4cb92e72ac0
Tim Reznich <tim.reznich@gmail.com>
2018-12-11 15:52:17 +0200

    improvement(ui): user profile page layout

--- ui/web/src/components/UserProfilePageLayout/UserProfilePageLayout.jsx
+++ ui/web/src/components/UserProfilePageLayout/UserProfilePageLayout.jsx
@@ -1,123 +1,45 @@
 // @flow
 import React from 'react';
+import * as S from './styled';
 import {
-  Responsive,
-  WidthProvider,
-} from 'react-grid-layout';
-
-
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
-
-const layoutXs = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 1, h: 22, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 0, y: 22, w: 1, h: 11, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 0, y: 33, w: 1, h: 11, static: true,
-  },
-];
-
-
-const layoutSm = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 2, h: 22, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 0, y: 22, w: 2, h: 11, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 0, y: 33, w: 2, h: 11, static: true,
-  },
-];
-
-const layoutMd = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 4.4, h: 22, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 5, y: 0, w: 5.6, h: 11, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 5, y: 11, w: 5.6, h: 11, static: true,
-  },
-];
-
-const layoutLg = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 6, h: 22, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 6, y: 0, w: 6, h: 11, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 6, y: 10, w: 6, h: 11, static: true,
-  },
-];
-
-const layoutLg2 = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 12, h: 25, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 12, y: 0, w: 12, h: 12.5, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 12, y: 12.5, w: 12, h: 12.5, static: true,
-  },
-];
-
-const layoutLg3 = [
-  {
-    i: 'userBalance', x: 0, y: 0, w: 6, h: 32, static: true,
-  },
-  {
-    i: 'userOpenOrders', x: 6, y: 0, w: 6, h: 16, static: true,
-  },
-  {
-    i: 'tradingHistory', x: 6, y: 16, w: 6, h: 16, static: true,
-  },
-];
+  renderLayout,
+} from '../TradingPageLayout/TradingPageLayout';
 
 type Props = {
   children: React.Node,
 };
 
+const userProfileColumns = [
+  {
+    rows: [
+      {
+        component: 'userBalance',
+        container: S.UserBalanceWrapper,
+      },
+    ],
+    container: S.Column1,
+    key: 'column1',
+  },
+  {
+    rows: [
+      {
+        component: 'tradingHistory',
+        container: S.TradingHistoryWrapper,
+      },
+      {
+        component: 'userOpenOrders',
+        container: S.UserOpenOrdersWrapper,
+      },
+    ],
+    container: S.Column2,
+    key: 'column2',
+  },
+];
+
 const UserProfilePageLayout = ({ children }: Props) => (
-  <ResponsiveReactGridLayout
-    layouts={{
-      xs: layoutXs,
-      sm: layoutSm,
-      md: layoutMd,
-      lg: layoutLg,
-      lg2: layoutLg2,
-      lg3: layoutLg3,
-    }}
-    breakpoints={{
-      xs: 0,
-      sm: 600,
-      md: 1000,
-      lg: 1350,
-      lg2: 1550,
-      lg3: 2000,
-    }}
-    cols={{
-      xs: 1,
-      sm: 2,
-      md: 10,
-      lg: 12,
-      lg2: 24,
-      lg3: 12,
-    }}
-    useCSSTransforms
-    width="100vh"
-    rowHeight={30}
-    onLayoutChange={() => {}}
-  >
-    {children}
-  </ResponsiveReactGridLayout>
+  <S.UserProfilePage>
+    {renderLayout(userProfileColumns, children)}
+  </S.UserProfilePage>
 );
 
 export default UserProfilePageLayout;
--- ui/web/src/components/UserProfilePageLayout/styled/index.js
+++ ui/web/src/components/UserProfilePageLayout/styled/index.js
@@ -8,3 +8,42 @@ export const DottedBorder = styled.div`
   font-size: 24px;
   background-color: ${colors['body-background']};
 `;
+
+export const UserProfilePage = styled.div`
+  display: flex;
+  height: calc(100vh - 54px);
+`;
+
+const Column = styled.div`
+  height: 100%;
+  padding: 0 0 5px 0;
+`;
+
+export const Column1 = styled(Column)`
+  flex-grow: 1;
+  flex-basis: auto;
+  min-width: 550px;
+`;
+
+export const Column2 = styled(Column)`
+  flex-grow: 1;
+  flex-basis: auto;
+  min-width: 750px;
+`;
+
+const Element = styled.div`
+  padding: 5px 5px 0 0;
+`;
+
+
+export const UserBalanceWrapper = styled(Element)`
+  height: 100%
+`;
+
+export const TradingHistoryWrapper = styled(Element)`
+  height: 60%;
+`;
+
+export const UserOpenOrdersWrapper = styled(Element)`
+  height: 40%;
+`;
